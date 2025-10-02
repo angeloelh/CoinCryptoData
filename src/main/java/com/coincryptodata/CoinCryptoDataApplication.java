904c29dd792d020e32f241fe56a0a17645d8262c
keysax0 <elhajj.angelo9@gmail.com>
2025-10-01 21:03:32 -0400

    First commit

---
+++ src/main/java/com/coincryptodata/CoinCryptoDataApplication.java
@@ -0,0 +1,13 @@
+package com.coincryptodata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinCryptoDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoinCryptoDataApplication.class, args);
+    }
+
+}
